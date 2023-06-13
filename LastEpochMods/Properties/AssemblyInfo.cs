73a120644609ba6650daa76737ca717a83b51afa
RCInet <contact.rci.net@gmail.com>
2023-06-13 20:02:50 +0200

    Dungeons No Key Require

--- LastEpochMods/Properties/AssemblyInfo.cs
+++ LastEpochMods/Properties/AssemblyInfo.cs
@@ -38,7 +38,7 @@ using MelonLoader;
 // Vous pouvez spécifier toutes les valeurs ou indiquer les numéros de build et de révision par défaut
 // en utilisant '*', comme indiqué ci-dessous :
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.274")]
-[assembly: AssemblyFileVersion("1.0.0.274")]
+[assembly: AssemblyVersion("1.0.0.278")]
+[assembly: AssemblyFileVersion("1.0.0.278")]
 
-[assembly: AssemblyInformationalVersion("0.0.0.274")]
+[assembly: AssemblyInformationalVersion("0.0.0.278")]
