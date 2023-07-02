7f19767904c32317b3f6f666f96a5db9d448637f
RCInet <contact.rci.net@gmail.com>
2023-07-03 00:03:31 +0200

    Clean code

--- LastEpochMods/Mods/Affixs_Mods.cs
+++ LastEpochMods/Mods/Affixs_Mods.cs
@@ -5,37 +5,18 @@ namespace LastEpochMods.Mods
     public class Affixs_Mods
     {
         private static AffixList affix_list = null;
-        //private static bool affixs_found = false;
-        /*private static void InitAffixsList()
-        {
-            if (!affixs_found)
-            {
-                try
-                {
-                    affix_list = AffixList.get();
-                    if (affix_list != null)
-                    {
-                        affixs_found = true;
-                        Main.logger_instance.Msg("Affixs item list found : " + affix_list.name);
-                    }
-                }
-                catch (System.Exception ex)
-                {
-                    Main.logger_instance.Msg("Error Affixs item list");
-                }
-            }
-        }*/
-
         public static bool Enable_Affixs_Multiplier = true;
         public static int Affixs_Multiplier = 10;
 
-        public static bool Enable_Edit_Affixs_Rolls = false;
+        public static void Launch()
+        {
+            Mods.Affixs_Mods.MultiplyAffixsRolls(Affixs_Multiplier);
+            //Mods.Affixs_Mods.EditAffixRollsByTier(100, 7, 100, 999); //Edit Affixs Exemple
+        }
         private static void MultiplyAffixsRolls(int mutiplier)
         {
             if (Enable_Affixs_Multiplier)
             {
-                //if (!affixs_found) { InitAffixsList(); }
-
                 affix_list = AffixList.get();
                 if (affix_list != null)
                 {
@@ -60,11 +41,13 @@ namespace LastEpochMods.Mods
                 }
             }
         }
+
+        #region Exemple
+        public static bool Enable_Edit_Affixs_Rolls = false;
         private static void EditAffixRollsByTier(int affix_id, int tier, int min, int max)
         {
             if (Enable_Edit_Affixs_Rolls)
-            {
-                //if (!affixs_found) { InitAffixsList(); }
+            {                
                 affix_list = AffixList.get();
                 if (affix_list != null)
                 {
@@ -101,10 +84,6 @@ namespace LastEpochMods.Mods
                 }
             }
         }
-        public static void Launch()
-        {
-            Mods.Affixs_Mods.MultiplyAffixsRolls(Affixs_Multiplier);
-            //Mods.Affixs_Mods.EditAffixRollsByTier(100, 7, 100, 999); //Edit Affixs Exemple
-        }
+        #endregion
     }
 }
