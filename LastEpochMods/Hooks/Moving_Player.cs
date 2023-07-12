d514d288950d721b783af7dc0298ad621fa4862b
RCInet <contact.rci.net@gmail.com>
2023-07-12 15:03:02 +0200

    Lock player Movement

---
+++ LastEpochMods/Hooks/Moving_Player.cs
@@ -0,0 +1,18 @@
+﻿using HarmonyLib;
+
+namespace LastEpochMods.Hooks
+{
+    public class Moving_Player
+    {
+        [HarmonyPatch(typeof(MovingPlayer), "MouseClickMoveCommand")]
+        public class ShardAdded
+        {
+            [HarmonyPrefix]
+            static bool Prefix(MovingPlayer __instance, UnityEngine.Ray __0, bool __1, float __2, bool __3, UnityEngine.Vector3 __4, bool __5)
+            {
+                if (Ui.Menu.lock_movement) { return false; }
+                else { return true; }
+            }
+        }
+    }
+}
