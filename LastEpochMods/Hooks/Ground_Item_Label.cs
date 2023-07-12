c5e20e77984c23079438211600ff248ac6ea1125
RCInet <contact.rci.net@gmail.com>
2023-07-12 15:03:24 +0200

    Range Pickup

---
+++ LastEpochMods/Hooks/Ground_Item_Label.cs
@@ -0,0 +1,22 @@
+﻿using HarmonyLib;
+
+namespace LastEpochMods.Hooks
+{
+    public class Ground_Item_Label
+    {
+        [HarmonyPatch(typeof(GroundItemLabel), "ClickedItem")]
+        public class ClickedItem
+        {
+            [HarmonyPrefix]
+            static bool Prefix(GroundItemLabel __instance)
+            {
+                if (Config.Data.mods_config.items.Enable_pickup_range)
+                {
+                    __instance.requestPickup();
+                    return false;
+                }
+                else { return true; }
+            }
+        }
+    }
+}
