80cf49122a9356d5cf177affe3f32f7194565b3f
RCInet <contact.rci.net@gmail.com>
2023-07-12 15:03:52 +0200

    Monoliths Enemy Density

---
+++ LastEpochMods/Hooks/Monolith_Timeline_Panel_Manager.cs
@@ -0,0 +1,21 @@
+﻿using HarmonyLib;
+
+namespace LastEpochMods.Hooks
+{
+    public class Monolith_Timeline_Panel_Manager
+    {
+        [HarmonyPatch(typeof(MonolithTimelinePanelManager), "Update")]
+        public class Update
+        {
+            [HarmonyPrefix]
+            static bool Prefix(MonolithTimelinePanelManager __instance)
+            {
+                if (Config.Data.mods_config.scene.Enable_Monolith_EnemyDensity)
+                {
+                    __instance.run.timeline.enemyDensityModifier = Config.Data.mods_config.scene.Monolith_EnemyDensity;
+                }
+                return true;
+            }
+        }
+    }
+}
