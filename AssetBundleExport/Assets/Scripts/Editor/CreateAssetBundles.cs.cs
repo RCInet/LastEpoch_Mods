01b1437c8f7b40ff45acc3934e0bdd4bf78e155f
RCInet <contact.rci.net@gmail.com>
2024-03-28 15:13:05 +0100

    UnityProject for Building Assets

---
+++ AssetBundleExport/Assets/Scripts/Editor/CreateAssetBundles.cs.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.IO;
+using UnityEditor;
+using UnityEngine;
+
+public class CreateAssetBundles
+{
+    [MenuItem("Assets/Create AssetBundles")]
+    static void BuildAllAssetBundles()
+    {
+        string assetBundleDirectory = Application.dataPath + "/../Out/FromScript";
+        if (!Directory.Exists(assetBundleDirectory)) { Directory.CreateDirectory(assetBundleDirectory); }
+        try
+        {            
+            BuildPipeline.BuildAssetBundles(assetBundleDirectory, BuildAssetBundleOptions.None, EditorUserBuildSettings.activeBuildTarget);
+        }
+        catch (Exception e) { Debug.LogWarning(e); }
+        
+		
+    }
+}
---
+++ AssetBundleExport/README.md
@@ -0,0 +1,4 @@
+# Default Unity Project, Allow create Assets for LastEpoch (Outup in "Out" folder)
++ 2 Ways :
+- With AssetBundle Manager
+- With Scrip (asset "Create AssetBundles", create all bundles)
