e168623d7a9a844e58caf44fcf43fb7e04a0be48
Sebastien Lafleur <sebastien.lafleur@gmail.com>
2026-04-06 12:21:18 -0400

    fixing Memory Amber dropping

--- LastEpoch_Hud/Scripts/Mods/Items/Items_AutoPickup_MemoryAmber.cs
+++ LastEpoch_Hud/Scripts/Mods/Items/Items_AutoPickup_MemoryAmber.cs
@@ -6,7 +6,7 @@ namespace LastEpoch_Hud.Scripts.Mods.Items
 {
     public class Items_AutoPickup_MemoryAmber
     {
-        /*public static bool CanRun()
+        public static bool CanRun()
         {
             if ((Scenes.IsGameScene()) && (!Save_Manager.instance.IsNullOrDestroyed()) && (!Refs_Manager.player_actor.IsNullOrDestroyed()))
             {
@@ -20,21 +20,21 @@ namespace LastEpoch_Hud.Scripts.Mods.Items
         public class PickupableObjectsManager_CreatePickupableObjectForPlayer2
         {
             [HarmonyPostfix]
-            static void Postfix(ref Il2CppLE.Factions.PickupableObjectsManager __instance, Il2CppLE.Factions.PickupableObjectSet __0, ref Vector3 __1, uint __2)
+            static void Postfix(ref Il2CppLE.Factions.PickupableObjectsManager __instance, Il2CppLE.Factions.PickupableObjectSet __1, ref Vector3 __2, uint __3)
             {
                 if (CanRun())
                 {
                     bool pick = false;
-                    System.UInt32 id = __0.NextID - 1;
-                    foreach (Il2CppSystem.Collections.Generic.KeyValuePair<Il2CppLE.Factions.PickupableObjectType, Il2CppSystem.Collections.Generic.List<Il2CppLE.Factions.PickupableObject>> obj in __0.pickupables)
+                    System.UInt32 id = __1.NextID - 1;
+                    foreach (Il2CppSystem.Collections.Generic.KeyValuePair<Il2CppLE.Factions.PickupableObjectType, Il2CppSystem.Collections.Generic.List<Il2CppLE.Factions.PickupableObject>> obj in __1.pickupables)
                     {
                         int index = 0;
                         foreach (Il2CppLE.Factions.PickupableObject pickupable_obj in obj.Value)
                         {
                             if (pickupable_obj.Id == id)
                             {
-                                __1 = Refs_Manager.player_actor.position();                                
-                                __instance.PickupObject(__0, pickupable_obj, index);
+                                __2 = Refs_Manager.player_actor.position();
+                                __instance.PickupObject(__1, pickupable_obj, index);
                                 pick = true;
                                 break;
                             }
@@ -44,6 +44,6 @@ namespace LastEpoch_Hud.Scripts.Mods.Items
                     }
                 }
             }
-        }*/
+        }
     }
 }
