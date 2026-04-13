699b96890d7cf3f950aba88b510e0fd649dac60d
PKozdra <piotr@kozdra.pl>
2026-04-13 22:09:11 +0200

    Change method in fps hud cap to skip graphics processor approach

--- LastEpoch_Hud/Scripts/Mods/Fixs/Fix_HudFpsCap.cs
+++ LastEpoch_Hud/Scripts/Mods/Fixs/Fix_HudFpsCap.cs
@@ -1,27 +1,14 @@
-using HarmonyLib;
-using Il2CppGraphicsBackend;
-using Il2CppLE.Settings;
 using UnityEngine;
 
 namespace LastEpoch_Hud.Scripts.Mods.Fixs
 {
     public class Fix_HudFpsCap
     {
-        static GraphicsSettingsProcessor processor;
         static int savedTargetFps = int.MinValue;
         const int FallbackTargetFps = 30;
 
         public static void SetHudCapActive(bool active)
         {
-            if (!processor.IsNullOrDestroyed())
-            {
-                try
-                {
-                    processor.TryApplyMenuFPSLimit(active);
-                    return;
-                }
-                catch { }
-            }
             if (active)
             {
                 if (savedTargetFps == int.MinValue)
@@ -34,20 +21,6 @@ namespace LastEpoch_Hud.Scripts.Mods.Fixs
             {
                 Application.targetFrameRate = savedTargetFps;
                 savedTargetFps = int.MinValue;
-            }
-        }
-
-        [HarmonyPatch(
-            typeof(GraphicsSettingsProcessor),
-            MethodType.Constructor,
-            new System.Type[] { typeof(GraphicsSettings) }
-        )]
-        public class GraphicsSettingsProcessor_ctor
-        {
-            [HarmonyPostfix]
-            static void Postfix(GraphicsSettingsProcessor __instance)
-            {
-                processor = __instance;
             }
         }
     }
